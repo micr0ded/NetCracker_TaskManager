3b32fff20bebba14990a2d1cac655527daf930e8
cherakaevs <marble462@gmail.com>
2021-11-28 17:51:55 +0400

    Initial commit

---
+++ src/main/java/task/Task.java
@@ -0,0 +1,42 @@
+package task;
+
+public class Task {
+    private int ID;
+    private String description;
+    private long time;
+
+    public Task(int taskID, String desc, long time){
+        ID = taskID;
+        description = desc;
+        this.time = time;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public String toString(){
+        String str = new String(ID + " " + description + " " + time);
+        return str;
+    }
+}
