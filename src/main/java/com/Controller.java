9966ecb383f2fd9aeb4672e38313c756d8bda947
cherakaevs <marble462@gmail.com>
2021-12-08 19:38:20 +0400

    update commit

--- src/main/java/com/Controller.java
+++ src/main/java/com/Controller.java
@@ -1,13 +1,36 @@
 package com;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class Controller {
+    private final Service service;
+
+    public Controller(Service service){
+        this.service = service;
+    }
+
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
+    }
+
+
+    @GetMapping("/tasks/{id}")
+    public Task getTask(@PathVariable(value = "id") int id) {
+        return service.getTask(id);
+    }
+
+    @PostMapping("/tasks")
+    public void addTask(@RequestBody Task task){
+        service.addTask(task);
+    }
+
+
+    @GetMapping("/tasks")
+    public List<Task> getAllTasks(){
+        return service.getAllTasks();
     }
 }
--- src/main/java/com/TaskBase.java
+++ src/main/java/com/TaskBase.java
@@ -1,8 +1,11 @@
 package com;
 
+import org.springframework.stereotype.Component;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class TaskBase {
     private List<Task> tasks;
 
